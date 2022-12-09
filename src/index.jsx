f2c58d4c434fbc43734d719899e02ba06cf2f227
wang-yurin <king_1994@naver.com>
2022-12-09 15:14:59 +0900

    Chore: Recoil 패키지 설치 및 Recoil root 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Reset } from 'styled-reset';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Reset />
-    <GlobalStyle />
-    <App />
+    <RecoilRoot>
+      <Reset />
+      <GlobalStyle />
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
