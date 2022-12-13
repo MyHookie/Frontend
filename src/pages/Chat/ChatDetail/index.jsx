48528a5935deca272ee0a47c773176081413d755
seolha <kynwww@naver.com>
2022-12-13 16:25:24 +0900

    Design: 헤더와 하단 입력 바 import(#16)

--- src/pages/Chat/ChatDetail/index.jsx
+++ src/pages/Chat/ChatDetail/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import BaseHeader from '../../../components/common/BaseHeader';
-import ConfirmHeader from '../../../components/common/ConfirmHeader';
 import MessageItem from '../../../components/MessageItem';
 import MessageItemYours from '../../../components/MessageItemYours';
+import leftIcon from '../../../assets/icon/icon-arrow-left.png';
+import rightIcon from '../../../assets/icon/s-icon-more-vertical.png';
+import MessageInputBar from '../../../components/MessageInput';
 
 const SContainer = styled.div``;
 
@@ -28,15 +30,18 @@ const dummyTime = {
   time3: '12:50',
 };
 
-function ChatDetail(props) {
+const nickName = '사용자 닉네임';
+
+function ChatDetail() {
   return (
     <SContainer>
-      <BaseHeader />
+      <BaseHeader leftIcon={leftIcon} title={nickName} rightIcon={rightIcon} />
       <SMessageList>
         <MessageItem text={dummyText.message1} time={dummyTime.time1} />
         <MessageItem text={dummyText.message2} time={dummyTime.time2} />
         <MessageItemYours text={dummyText.message3} time={dummyTime.time3} />
       </SMessageList>
+      <MessageInputBar />
     </SContainer>
   );
 }
