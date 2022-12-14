9bcca22035862c67d23176465b27af6eb784f651
wang-yurin <king_1994@naver.com>
2022-12-14 13:11:51 +0900

    Feat: 인풋창 입력 값 signup 페이지 내 state 관리 - signup 페이지에서 상태관리를 위해 useContext 훅 사용 - useContext 사용 시 useMemo 훅과 함께 사용하라는 에러메시지로 useMemo 적용

--- src/pages/Auth/SignUp/index.jsx
+++ src/pages/Auth/SignUp/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import Title from '../../../components/Title';
 import FormInput from '../../../components/FormInput';
@@ -18,46 +18,77 @@ const FormContainer = styled.form`
   gap: 3rem;
 `;
 
+//
+
 const handleButtonClick = () => console.log('click');
+
+// useContext Hook 사용
+const initialFormData = {
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
+export const FormContext = createContext({
+  formData: initialFormData,
+  setFormData: () => {},
+});
+
+// useContext 사용 시 useMemo 사용하라는 error 발생, 리렌더링 방지
+function FormContextProvider({ children }) {
+  const [formData, setFormData] = useState(initialFormData);
+
+  const value = useMemo(
+    () => ({
+      formData,
+      setFormData,
+    }),
+    [formData]
+  );
+
+  return <FormContext.Provider value={value}>{children}</FormContext.Provider>;
+}
 
 function SignUp() {
   return (
-    <SContainer>
-      <Title leftIcon={leftIcon} handleButtonClick={handleButtonClick}>
-        이메일로 회원가입
-      </Title>
-      <FormContainer>
-        <FormInput
-          id="email"
-          label="이메일"
-          inputProps={{
-            type: 'email',
-            placeholder: '이메일 주소를 입력해주세요.',
-          }}
-          warningMsg="* 이미 가입된 이메일 주소입니다."
-          isValide
-        />
-        <FormInput
-          id="password"
-          label="비밀번호"
-          inputProps={{
-            type: 'password',
-            placeholder: '비밀번호를 입력해주세요.',
-            autoComplete: 'off',
-          }}
-        />
-        <FormInput
-          id="checkedPassword"
-          label="비밀번호 확인"
-          inputProps={{
-            type: 'password',
-            placeholder: '비밀번호를 한번 더 입력해주세요.',
-            autoComplete: 'off',
-          }}
-        />
-        <Button text="회원가입" buttonStyle={LARGE_BUTTON} />
-      </FormContainer>
-    </SContainer>
+    <FormContextProvider>
+      <SContainer>
+        <Title leftIcon={leftIcon} handleButtonClick={handleButtonClick}>
+          이메일로 회원가입
+        </Title>
+        <FormContainer>
+          <FormInput
+            id="email"
+            label="이메일"
+            inputProps={{
+              type: 'email',
+              placeholder: '이메일 주소를 입력해주세요.',
+            }}
+            warningMsg="* 이미 가입된 이메일 주소입니다."
+            isValid
+          />
+          <FormInput
+            id="password"
+            label="비밀번호"
+            inputProps={{
+              type: 'password',
+              placeholder: '비밀번호를 입력해주세요.',
+              autoComplete: 'off',
+            }}
+          />
+          <FormInput
+            id="confirmPassword"
+            label="비밀번호 확인"
+            inputProps={{
+              type: 'password',
+              placeholder: '비밀번호를 한번 더 입력해주세요.',
+              autoComplete: 'off',
+            }}
+          />
+          <Button text="회원가입" buttonStyle={LARGE_BUTTON} />
+        </FormContainer>
+      </SContainer>
+    </FormContextProvider>
   );
 }
 
