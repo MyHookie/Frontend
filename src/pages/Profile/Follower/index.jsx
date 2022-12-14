cac5bb2a4786943db73d21a73503d4e4b1ce4b67
yoorimseo <seou1225@naver.com>
2022-12-14 15:19:43 +0900

    Feature: FollowerItem 컴포넌트를 사용하여 팔로우 리스트를 불러오도록 구현 (#32)

--- src/pages/Profile/Follower/index.jsx
+++ src/pages/Profile/Follower/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import BaseHeader from '../../../components/common/BaseHeader';
-import Button from '../../../components/common/Button';
-import { FOLLOW_BUTTON } from '../../../constants/buttonStyle';
+import FollowerItem from '../../../components/FollowerItem';
 
 import arrowIcon from '../../../assets/icon/icon-arrow-left.png';
-import basicProfilSmallImg from '../../../assets/basic-profile_small.png';
-import { IR, slEllipsis } from '../../../styles/Util';
+import { IR } from '../../../styles/Util';
+import dummyList from '../../../components/FollowerItem/dummyList';
 
 const SContainer = styled.section`
   padding: 24px 16px 0;
@@ -16,97 +15,31 @@ const STitle = styled.h2`
   ${IR}
 `;
 
-const SContent = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 1.6rem;
-  align-items: center;
-`;
-
-const STextItem = styled.div`
-  margin: 0 1.2rem;
-`;
-
-const SImg = styled.img`
-  width: 5rem;
-`;
-
-const STextUserId = styled.p`
-  width: 22.8rem;
-  margin-bottom: 0.6rem;
-  font-size: 1.4rem;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
-
-const STextIntroduction = styled.p`
-  width: 22.8rem;
-  font-size: 1.2rem;
-  color: ${({ theme }) => theme.color.GRAY};
-  ${slEllipsis}
-`;
-
 function Follower() {
   return (
     <div>
       <BaseHeader leftIcon={arrowIcon} title="Follower" />
       <SContainer>
         <STitle>팔로워 페이지</STitle>
-        <SContent>
-          <SImg src={basicProfilSmallImg} alt="프로필 이미지" />
-          <STextItem>
-            <STextUserId>
-              안녕하세요, 사용자 이름입니다. 이러쿵 저러쿵 어쩌고 저쩌고
-            </STextUserId>
-            <STextIntroduction>
-              안녕하세요, 제 소개를 하자면요. 이러쿵 저러쿵 어쩌고 저쩌고
-            </STextIntroduction>
-          </STextItem>
-          <Button text="팔로우" buttonStyle={FOLLOW_BUTTON} />
-        </SContent>
-
-        <SContent>
-          <SImg src={basicProfilSmallImg} alt="프로필 이미지" />
-          <STextItem>
-            <STextUserId>userId</STextUserId>
-            <STextIntroduction>Hello, world!</STextIntroduction>
-          </STextItem>
-          <Button
-            text="팔로우"
-            buttonStyle={FOLLOW_BUTTON}
-            color="green"
-            background="pink"
-          />
-        </SContent>
-
-        <SContent>
-          <SImg src={basicProfilSmallImg} alt="프로필 이미지" />
-          <STextItem>
-            <STextUserId>
-              안녕하세요, 사용자 이름입니다. 이러쿵 저러쿵 어쩌고 저쩌고
-            </STextUserId>
-            <STextIntroduction>
-              안녕하세요, 제 소개를 하자면요. 이러쿵 저러쿵 어쩌고 저쩌고
-            </STextIntroduction>
-          </STextItem>
-          <Button cancel text="취소" buttonStyle={FOLLOW_BUTTON} />
-        </SContent>
-
-        <SContent>
-          <SImg src={basicProfilSmallImg} alt="프로필 이미지" />
-          <STextItem>
-            <STextUserId>userId</STextUserId>
-            <STextIntroduction>Hello, world!</STextIntroduction>
-          </STextItem>
-          <Button
-            cancel
-            text="취소"
-            buttonStyle={FOLLOW_BUTTON}
-            color="green"
-            background="pink"
-          />
-        </SContent>
+        {dummyList.map((item) =>
+          item.isfollow ? (
+            <FollowerItem
+              key={item.id}
+              username={item.username}
+              intro={item.intro}
+              image={item.image}
+              state="취소"
+            />
+          ) : (
+            <FollowerItem
+              key={item.id}
+              username={item.username}
+              intro={item.intro}
+              image={item.image}
+              state="팔로우"
+            />
+          )
+        )}
       </SContainer>
     </div>
   );
