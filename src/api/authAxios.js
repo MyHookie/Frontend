f45dec8dcc331df2d3b9f6b676404b21d6fcebcb
wang-yurin <king_1994@naver.com>
2022-12-18 22:44:49 +0900

    Feat: authAxios에 headers 추가

--- src/api/authAxios.js
+++ src/api/authAxios.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const authAxios = axios.create({
   baseURL: 'https://mandarin.api.weniv.co.kr/user',
+  headers: {
+    'Content-type': 'application/json',
+  },
 });
 
 export default authAxios;
