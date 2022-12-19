4d478d9dab458643252f698ebf3277580b36a45e
dev-yun <yunpro2766@gmail.com>
2022-12-19 16:07:35 +0900

    Dir: Upload페이지의 styled-component와 로직 코드 분리

--- src/pages/Post/PostUpload/index.jsx
+++ src/pages/Post/PostUpload/index.jsx
@@ -2,111 +2,10 @@ import React, { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { nanoid } from 'nanoid';
 import axios from 'axios';
-import styled from 'styled-components';
+import * as S from './index.styles';
 import ConfirmHeader from '../../../components/common/ConfirmHeader';
 import TagItem from './TagItem';
 import deleteButton from '../../../assets/icon/x_shadow.png';
-
-const SContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  padding: 0 1.4rem;
-`;
-
-const STagContainer = styled.div`
-  padding: 1rem 0rem;
-
-  form {
-    margin-bottom: 1rem;
-  }
-`;
-
-const STagInput = styled.input`
-  width: 100%;
-  padding: 1.2rem 0.8rem;
-  border: none;
-  font-size: ${({ theme }) => theme.fontSize.MEDIUM};
-
-  border-radius: 1.5rem;
-  &::placeholder {
-    color: ${({ theme }) => theme.color.LIGHT_GRAY};
-  }
-`;
-
-const STagList = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 0.5rem;
-
-  min-height: 3.5rem;
-  width: 100%;
-  padding-bottom: 1rem;
-
-  border-bottom: 1px solid ${({ theme }) => theme.color.LIGHT_GRAY};
-`;
-
-const SImageContainer = styled.div`
-  display: flex;
-  flex-wrap: nowrap;
-  overflow-x: scroll;
-
-  height: 10.4rem;
-  padding-bottom: 1rem;
-  box-sizing: content-box;
-
-  border-bottom: 1px solid ${({ theme }) => theme.color.LIGHT_GRAY};
-
-  gap: 1rem;
-`;
-
-const SPreviewImageBox = styled.div`
-  position: relative;
-
-  width: 10.4rem;
-  min-width: 10.4rem;
-  border-radius: 1.5rem;
-  border: 1px solid ${({ theme }) => theme.color.LIGHT_GRAY};
-
-  overflow: hidden;
-
-  img {
-    height: 100%;
-    object-fit: cover;
-  }
-  button {
-    position: absolute;
-    top: 0.5rem;
-    right: 0.5rem;
-    width: 1.8rem;
-  }
-`;
-
-const SImageInput = styled.div`
-  height: 100%;
-  width: 10.4rem;
-
-  flex: 0 0 auto;
-  font-size: 3.6rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  border-radius: 1.5rem;
-  color: ${({ theme }) => theme.color.GRAY};
-  background-color: ${({ theme }) => theme.color.LIGHT_GRAY};
-`;
-
-const SContent = styled.textarea`
-  width: 100%;
-  height: 30vh;
-  font-size: ${({ theme }) => theme.fontSize.MEDIUM};
-  line-height: 1.8rem;
-  resize: none;
-
-  margin-top: 2rem;
-  padding: 0rem 1rem;
-`;
 
 function PostUpload() {
   const [tag, setTags] = useState('');
@@ -237,16 +136,16 @@ function PostUpload() {
   return (
     <>
       <ConfirmHeader leftClick={goBackPage} rightClick={createPost} />
-      <SContainer>
-        <STagContainer>
-          <STagInput
+      <S.Container>
+        <S.TagContainer>
+          <S.TagInput
             type="text"
             value={tag}
             onChange={handleTagChange}
             onKeyDown={handleTagPush}
             placeholder="#장소 #위치 #카테고리"
           />
-          <STagList>
+          <S.TagList>
             {tagList.map((tagText, index) => (
               <TagItem
                 key={nanoid()}
@@ -254,11 +153,11 @@ function PostUpload() {
                 handleTagDelete={() => handleTagDelete(index)}
               />
             ))}
-          </STagList>
-        </STagContainer>
+          </S.TagList>
+        </S.TagContainer>
         <form>
-          <SImageContainer>
-            <SImageInput onClick={handleImageAdd}>+</SImageInput>
+          <S.ImageContainer>
+            <S.ImageInput onClick={handleImageAdd}>+</S.ImageInput>
             <input
               ref={imageInput}
               type="file"
@@ -269,7 +168,7 @@ function PostUpload() {
             />
             {base64Image &&
               base64Image.map((src, index) => (
-                <SPreviewImageBox key={nanoid()}>
+                <S.PreviewImageBox key={nanoid()}>
                   <img src={src} alt="미리보기 이미지" />
                   <button
                     type="button"
@@ -277,17 +176,17 @@ function PostUpload() {
                   >
                     <img src={deleteButton} alt="미리보기 이미지 삭제" />
                   </button>
-                </SPreviewImageBox>
+                </S.PreviewImageBox>
               ))}
-          </SImageContainer>
-          <SContent
+          </S.ImageContainer>
+          <S.Content
             type="text"
             placeholder="후기를 입력해주세요!"
             value={content}
             onChange={handleContentChange}
           />
         </form>
-      </SContainer>
+      </S.Container>
     </>
   );
 }
