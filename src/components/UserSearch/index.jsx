e9b3c771c2c1dc9c4247b065c1914c706b88127c
dev-yun <yunpro2766@gmail.com>
2022-12-19 22:38:34 +0900

    Dir: Homepage의 Search 컴포넌트 분리

--- src/components/UserSearch/index.jsx
+++ src/components/UserSearch/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import SearchHeader from '../common/SearchHeader';
 
 const SContainer = styled.div`
   height: calc(100 + 4.4) vh;
   background-color: ${({ theme }) => theme.color.WHITE};
 `;
 
-function UserSearch() {
-  return <SContainer>api 통신</SContainer>;
+function UserSearch({ handleSearchActive }) {
+  return (
+    <>
+      <SearchHeader leftClick={handleSearchActive} />
+      <SContainer>api 통신</SContainer>;
+    </>
+  );
 }
 
 export default UserSearch;
