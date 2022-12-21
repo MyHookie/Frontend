b20c6bbfa3b2bb2e6ae7a355ebbc1feb431a6953
wang-yurin <king_1994@naver.com>
2022-12-21 16:55:53 +0900

    Design: ImageInput 컴포넌트 CSS 수정 - styled-component div에서 img로 변경 - img 변경으로 기존 div에서 사용했던 가상요소를 사용하지 못해 CSS 수정

--- src/components/ProfileImageInput/index.style.js
+++ src/components/ProfileImageInput/index.style.js
@@ -2,25 +2,25 @@ import styled from 'styled-components';
 import uploadIcon from '../../assets/upload-file.png';
 
 export const Container = styled.div`
-  margin: 2rem 0 3rem;
-  width: 100%;
-`;
-
-export const ImageInput = styled.div`
   position: relative;
-  width: 12rem;
-  height: 12rem;
-  margin: 0 auto;
-  border-radius: ${({ theme }) => theme.borderRadius.ROUND};
+  margin: 2rem 0 3rem;
 
   &::after {
     content: '';
     position: absolute;
-    right: 0;
-    bottom: 0;
+    bottom: 2rem;
+    right: 7rem;
     width: 4rem;
     height: 4rem;
     background-image: url(${uploadIcon});
     background-size: cover;
   }
+`;
+
+export const ImageInput = styled.img`
+  display: block;
+  width: 12rem;
+  height: 12rem;
+  margin: 0 auto;
+  border-radius: ${({ theme }) => theme.borderRadius.ROUND};
 `;
