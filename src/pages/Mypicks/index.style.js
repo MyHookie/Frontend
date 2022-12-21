35d840680526d3c99a950bac595986ef3f37960e
seolha <kynwww@naver.com>
2022-12-21 23:08:35 +0900

    Refactor: CSS 리팩토링(#72)

--- src/pages/Mypicks/index.style.js
+++ src/pages/Mypicks/index.style.js
@@ -1,21 +1,30 @@
 import styled from 'styled-components';
 import uploadIconGrey from '../../assets/upload-file_grey.png';
+import { IR } from '../../styles/Util';
 
 export const Container = styled.main`
-  height: calc(100 + 4.4) vh;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  padding: 6.2rem 3.4rem 0;
-  gap: 1.6rem;
+  gap: 1rem;
+  height: calc(100 + 4.4) vh;
+  padding: 6.2rem 3.4rem 3rem;
 `;
+
 export const ImageContainer = styled.div`
   position: relative;
   height: 100%;
   padding-bottom: 1rem;
   border-bottom: 1px solid ${({ theme }) => theme.color.LIGHT_GRAY};
   cursor: pointer;
-
+`;
+export const ImageInput = styled.div`
+  display: flex;
+  flex: 0 0 auto;
+  width: 100%;
+  height: 20.4rem;
+  border-radius: 1.5rem;
+  background-color: #f2f2f2;
+  border: 1px solid ${({ theme }) => theme.color.LIGHT_GRAY};
   &::after {
     content: '';
     position: absolute;
@@ -26,18 +35,6 @@ export const ImageContainer = styled.div`
     background-image: url(${uploadIconGrey});
     background-size: cover;
   }
-`;
-export const ImageInput = styled.div`
-  height: 20.4rem;
-  width: 32.2rem;
-  flex: 0 0 auto;
-  font-size: 3.6rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 1.5rem;
-  background-color: #f2f2f2;
-  border: 1px solid ${({ theme }) => theme.color.LIGHT_GRAY};
 `;
 export const Imgtxt = styled.p`
   position: absolute;
@@ -51,34 +48,57 @@ export const Label = styled.label`
   font-size: ${({ theme }) => theme.fontSize.SMALL};
   color: ${({ theme }) => theme.color.GRAY};
 `;
+
+// textarea, label에 fontfamily 임시 적용중
+
 export const Textarea = styled.textarea`
+  font-family: 'LINESeedKR-Rg';
   display: block;
-  margin-top: 10px;
-  width: 32.2rem;
+  margin-top: 0.3rem;
+  width: 100%;
   height: 25px;
   font-size: ${({ theme }) => theme.fontSize.MEDIUM};
   border-bottom: 1px solid ${({ theme }) => theme.color.LIGHT_GRAY};
   &::-webkit-scrollbar {
     display: none;
   }
+  &::placeholder {
+    font-family: 'LINESeedKR-Rg';
+    color: ${({ theme }) => theme.color.LIGHT_GRAY};
+  }
 `;
 
 export const LabelCheckBox = styled.label`
+  position: relative;
   display: flex;
   align-items: center;
-  user-select: none;
   width: 30%;
-`;
+  user-select: none;
+  cursor: default;
+  font-family: 'LINESeedKR-Rg';
+  letter-spacing: -0.3px;
 
-export const Checkbox = styled.input`
-  appearance: none;
-  width: 1.5rem;
-  height: 1.5rem;
-  border: 1.5px solid gainsboro;
-  border-radius: 0.35rem;
-
-  &:checked {
-    border-color: transparent;
+  &::before {
+    display: block;
+    content: '';
+    height: 1.5rem;
+    width: 1.5rem;
+    background-color: white;
+    border: 2px solid gainsboro;
+    border-radius: 0.35rem;
+  }
+  &::after {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+    display: block;
+    opacity: 0;
+    content: '';
+    height: 1.5rem;
+    width: 1.5rem;
+    border: 2px solid transparent;
+    border-radius: 0.35rem;
     background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
     background-size: 100% 100%;
     background-position: 50%;
@@ -87,6 +107,17 @@ export const Checkbox = styled.input`
   }
 `;
 
+export const Checkbox = styled.input`
+  ${IR}
+  &:checked + ${LabelCheckBox} {
+    ::after {
+      opacity: 1;
+    }
+  }
+`;
+
 export const StyledP = styled.p`
   margin-left: 0.5rem;
+  font-size: ${({ theme }) => theme.fontSize.SMALL};
+  color: ${({ theme }) => theme.color.GRAY};
 `;
