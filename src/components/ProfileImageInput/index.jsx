eee026c8db0f53150150e5e0e6af1ed863937aa8
wang-yurin <king_1994@naver.com>
2022-12-21 17:58:55 +0900

    Feat: 프로필 이미지 업로드 기능 구현 - 이미지를 업로드하면 프로필 화면에 표시

--- src/components/ProfileImageInput/index.jsx
+++ src/components/ProfileImageInput/index.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import * as S from './index.style';
-import profileImage from '../../assets/basic-profile.png';
+import basicProfileImage from '../../assets/basic-profile.png';
 
 function ProfileImageInput() {
+  const [profileImage, setProfileImage] = useState(basicProfileImage);
+  const imageInput = useRef(null);
+
+  const handleImageChange = (e) => {
+    if (e.target.files[0]) {
+      setProfileImage(e.target.files[0]);
+    } else {
+      setProfileImage(basicProfileImage); // 이미지 업로드 취소할 경우 기본 프로필 이미지로 설정
+    }
+
+    // 업로드 이미지 preview
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setProfileImage(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
   return (
-    <S.Container>
-      <S.ImageInput>
-        <img src={profileImage} alt="프로필 이미지" />
-      </S.ImageInput>
+    <S.Container
+      onClick={() => {
+        imageInput.current.click();
+      }}
+    >
+      <S.ImageInput src={profileImage} />
+      <input
+        type="file"
+        accept="image/jpg, image/jpeg, image/png, image/gif, image/bmp, image/tif, image/heic"
+        ref={imageInput}
+        style={{ display: 'none' }}
+        onChange={handleImageChange}
+      />
     </S.Container>
   );
 }
