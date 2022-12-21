a1a4ece4a51acabf1ea9fd7b86ff437dc316222e
seolha <kynwww@naver.com>
2022-12-21 13:40:22 +0900

    Feat: ConfirmHeader 기능 추가(#57) 화살표 아이콘 클릭 시 뒤로가기, 저장 버튼 클릭 시 Dialog 띄우기

--- src/pages/Mypicks/index.jsx
+++ src/pages/Mypicks/index.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ConfirmHeader from '../../components/common/ConfirmHeader';
 import leftIcon from '../../assets/icon/icon-arrow-left.png';
+import Dialog from '../../components/Modal/Dialog';
 
 function index() {
+  const navigate = useNavigate();
+  const goBackPage = () => {
+    navigate(-1);
+  };
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleDialogOpen = (e) => {
+    e.stopPropagation();
+    setIsDialogOpen(!isDialogOpen);
+  };
+
+  const handleSubmit = () => {
+    console.log('myPick 등록');
+    goBackPage();
+  };
+
   return (
     <div>
-      <ConfirmHeader leftIcon={leftIcon} />
+      <ConfirmHeader
+        leftIcon={leftIcon}
+        leftClick={goBackPage}
+        rightClick={handleDialogOpen}
+      />
+      {isDialogOpen && (
+        <Dialog
+          handleClose={handleDialogOpen}
+          handleSubmit={handleSubmit}
+          dialogText="저장하시겠습니까?"
+        />
+      )}
     </div>
   );
 }
