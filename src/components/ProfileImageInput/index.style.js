94aae111b705f840e276819b1539bca21f3b246a
wang-yurin <king_1994@naver.com>
2022-12-22 18:34:12 +0900

    Design: 프로필 이미지 스타일 수정

--- src/components/ProfileImageInput/index.style.js
+++ src/components/ProfileImageInput/index.style.js
@@ -24,5 +24,6 @@ export const ImageInput = styled.img`
   height: 12rem;
   margin: 0 auto;
   border-radius: ${({ theme }) => theme.borderRadius.ROUND};
+  object-fit: cover;
   cursor: pointer;
 `;
