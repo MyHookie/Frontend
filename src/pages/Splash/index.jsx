2be4fa482607fafb90e01abab8ad9964b1b4ef05
wang-yurin <king_1994@naver.com>
2022-12-24 14:53:04 +0900

    Design: fade out 애니메이션 효과 적용

--- src/pages/Splash/index.jsx
+++ src/pages/Splash/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './index.style';
 import speechBubbleImage from '../../assets/speech-bubble.png';
 
--- src/pages/Splash/index.style.js
+++ src/pages/Splash/index.style.js
@@ -15,6 +15,17 @@ const bounceIn = keyframes`
 }
 `;
 
+const fadeOut = keyframes`
+0% {
+  opacity: 1;
+  transform: translateY(0px);
+}
+100% {
+  opacity: 0;
+  transform: translateY(-100px);
+}
+`;
+
 export const Container = styled.div`
   width: 390px;
   height: 100vh;
@@ -29,7 +40,7 @@ export const ImageContainer = styled.div`
 export const SplashImage = styled.img`
   display: block;
   width: 30rem;
-  animation: ${bounceIn} 1s;
+  animation: ${bounceIn} 1s, ${fadeOut} 1.5s 1.1s;
   transform: scale(1);
   transition: all 0.4s cubic-bezier(0.8, 1.8, 0.75, 0.75);
 `;
