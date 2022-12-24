aa1e65be68a361eb4a42f7f3c07f439587d967e4
dev-yun <yunpro2766@gmail.com>
2022-12-24 15:50:31 +0900

    Feat: 프로필 UI 설정을 위한 데이터 GET API 작성

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+
+import BaseHeader from '../../components/common/BaseHeader';
 import Navigation from '../../components/common/Navigation';
+import leftArrowIcon from '../../assets/icon/icon-arrow-left.png';
+import verticalIcon from '../../assets/icon/s-icon-more-vertical.png';
+
+import getProfileInfo from '../../api/profile';
 
 function Profile() {
   const param = useParams();
   console.log(param);
+
+  const { data, isLoading, isError } = useQuery('profileInfo', () =>
+    getProfileInfo(param.id)
+  );
+
+  if (!isLoading) {
+    console.log(data.profile);
+  }
+
   return (
-    <div>
-      Profile
+    <>
+      <BaseHeader
+        leftIcon={leftArrowIcon}
+        leftClick={() => {}}
+        rightIcon={verticalIcon}
+        rightAlt="프로필 설정"
+        rightClick={() => {}}
+      />
+      {!isLoading && <div>{data.profile.accountname}</div>}
+
       <Navigation />
-    </div>
+    </>
   );
 }
 
