4c989ada18a700640112fa971a04e701ff511326
wang-yurin <king_1994@naver.com>
2022-12-25 12:45:02 +0900

    Feat: 스플래시 화면 전환 기능 추가 (#88) - 토큰이 있으면 홈 페이지, 토큰이 없으면 웰컴 페이지로 이동 - 버그: 토큰이 있을 때 스플래시 화면 이후 웰컴 페이지가 잠시 떴다가 홈 페이지로 이동

--- src/pages/Splash/index.jsx
+++ src/pages/Splash/index.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as S from './index.style';
 import speechBubbleImage from '../../assets/speech-bubble.png';
+import checkTokenValid from '../../api/tokenValid';
 
 function Splash() {
+  const navigate = useNavigate();
+  const [isToken, setIsToken] = useState(false);
+
+  useEffect(() => {
+    (async function checkToken() {
+      return (await checkTokenValid()) ? setIsToken(true) : setIsToken(false);
+    })();
+  }, []);
+
+  useEffect(() => {
+    setTimeout(
+      () => (!isToken ? navigate('/welcome') : navigate('/home')),
+      2000
+    );
+  }, [isToken]);
+
   return (
     <S.Container>
       <S.ImageContainer>
