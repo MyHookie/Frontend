2a67b9de025373861f0f0592ef415f33ebb50245
dev-yun <yunpro2766@gmail.com>
2022-12-27 00:11:44 +0900

    Feat: accountName, userName 유효성 검사, 에러메세지를 recoil selector로 관리하도록 변경

--- src/atoms/profileInfo.js
+++ src/atoms/profileInfo.js
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export const profileUserName = atom({
   key: 'profileUserName',
@@ -20,12 +20,46 @@ export const profileIntro = atom({
   default: '',
 });
 
-export const profileAccountNameValid = atom({
-  key: 'profileAccountNameValid',
-  default: false,
+export const isSubscribedAccountState = atom({
+  key: 'isSubscribedAccountState',
+  default: {
+    accountNameValid: true,
+    validWarningMessage: '',
+  },
 });
 
-export const profileAccountNameWarningMessage = atom({
-  key: 'profileAccountNameWarningMessage',
-  default: '',
+export const profileInputValid = selector({
+  key: 'profileInputValid',
+  get: ({ get }) => {
+    const userName = get(profileUserName);
+    const accountName = get(profileAccountName);
+    let accountNameValid;
+    let userNameValid;
+    let accountNameWarningMessage;
+    let userNameWarningMessage;
+
+    const ID_REGEX = /^[a-z0-9A-Z_.]{2,16}$/;
+
+    if (ID_REGEX.test(accountName)) {
+      accountNameValid = true;
+    } else {
+      accountNameValid = false;
+      accountNameWarningMessage =
+        '* 2~16자 이내의 영문, 숫자, 밑줄, 마침표만 사용할 수 있습니다.';
+    }
+
+    if (userName.length < 2 || userName.length > 10) {
+      userNameValid = false;
+      userNameWarningMessage = '* 2~10자 이내로 입력해주세요';
+    } else {
+      userNameValid = true;
+    }
+
+    return {
+      accountNameValid,
+      userNameValid,
+      accountNameWarningMessage,
+      userNameWarningMessage,
+    };
+  },
 });
