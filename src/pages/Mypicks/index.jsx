551adca2ef905f43d9e0ba3374643b98cca4d89f
seolha <kynwww@naver.com>
2022-12-26 22:58:26 +0900

    Style: 가격 Input disabled에서 readonly로 attr 변경 및 코드 스타일 수정

--- src/pages/Mypicks/index.jsx
+++ src/pages/Mypicks/index.jsx
@@ -9,25 +9,24 @@ import * as S from './index.style';
 function index() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [check, setCheck] = useState(false);
-  const [disabled, setDisabled] = useState(false);
+  const [readOnly, setReadOnly] = useState(false);
+
   const [imgFile, setImgFile] = useState('');
   const [value, setValue] = useState('');
   const [price, setPrice] = useState('');
   const [link, setLink] = useState('');
 
-  const [isCorrect, setIsCorrect] = useState(true);
+  const textRef = useRef();
+  const imageInput = useRef();
 
   const navigate = useNavigate();
   const goBackPage = () => {
     navigate(-1);
   };
 
-  const textRef = useRef();
-  const imageInput = useRef();
-
   const handleResizeHeight = useCallback((e) => {
     e.target.style.height = 'inherit';
-    e.target.style.height = `${e.target.scrollHeight + 1}px`;
+    e.target.style.height = `${e.target.scrollHeight + 2}px`;
   }, []);
 
   const handleDialogOpen = (e) => {
@@ -35,9 +34,21 @@ function index() {
     setIsDialogOpen(!isDialogOpen);
   };
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImgFile(reader.result);
+    };
+  };
+  const handleImageAdd = () => {
+    imageInput.current.click();
+  };
+
   // 폼 제출 유효성 검사
   const handleSubmit = () => {
-    if (disabled === true) {
+    if (readOnly === true) {
       setPrice('0');
     }
 
@@ -79,24 +90,12 @@ function index() {
     setCheck(e.target.checked);
     if (check) {
       setPlaceholderText('숫자만 입력 가능합니다.');
-      setDisabled(false);
+      setReadOnly(false);
     } else {
       setPrice('');
       setPlaceholderText('');
-      setDisabled(true);
+      setReadOnly(true);
     }
-  };
-
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      setImgFile(reader.result);
-    };
-  };
-  const handleImageAdd = () => {
-    imageInput.current.click();
   };
 
   return (
@@ -128,42 +127,45 @@ function index() {
           />
           {imgFile && <S.img src={imgFile} alt="mypick 사진" />}
         </S.ImageContainer>
-        <S.Label htmlFor="something1">한줄평</S.Label>
+        <S.Label htmlFor="review">한줄평</S.Label>
         <S.Textarea
           onChange={handleValueChange}
           ref={textRef}
           value={value}
           name=""
-          id="something1"
+          id="review"
           cols="30"
           rows="1"
           maxLength="100"
           placeholder="한줄평을 남겨주세요. (100자)"
         />
 
-        <S.Label htmlFor="something2">가격</S.Label>
-
+        <S.Label htmlFor="price">가격</S.Label>
         <S.Textarea
           value={price}
           onChange={handlePriceChange}
-          readOnly={disabled}
+          readOnly={readOnly}
           name=""
-          id="something2"
+          id="price"
           cols="30"
           rows="1"
           placeholder={placeholderText}
         />
-        <S.Checkbox onClick={handleCheck} type="checkbox" name="" id="price" />
-        <S.LabelCheckBox htmlFor="price">
+        <S.Checkbox
+          onClick={handleCheck}
+          type="checkbox"
+          name=""
+          id="read-only"
+        />
+        <S.LabelCheckBox htmlFor="read-only">
           <S.StyledP>가격 미정</S.StyledP>
         </S.LabelCheckBox>
-        <S.Label htmlFor="something3">링크</S.Label>
+        <S.Label htmlFor="link">링크</S.Label>
         <S.Textarea
           value={link}
           onChange={handleLinkChange}
-          isCorrect={isCorrect}
           name=""
-          id="something3"
+          id="link"
           cols="30"
           rows="1"
           placeholder="http://naver.com"
