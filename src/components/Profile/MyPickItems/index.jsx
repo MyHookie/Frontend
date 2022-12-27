50086be4e28f8de52d85f4ec71ffc922417deb36
seolha <kynwww@naver.com>
2022-12-27 13:47:05 +0900

    Design: UI 레이아웃 완성(#104)

--- src/components/Profile/MyPickItems/index.jsx
+++ src/components/Profile/MyPickItems/index.jsx
@@ -2,7 +2,34 @@ import React from 'react';
 import * as S from './index.style';
 
 function MyPickItems() {
-  return <div>index</div>;
+  return (
+    <S.Container>
+      <S.Title> MyPicks</S.Title>
+      <S.Items>
+        <S.Item>
+          <S.ImgContainer>
+            <S.Img />
+          </S.ImgContainer>
+          <S.ItemTitle>애월읍 노지노지노지노지노지 감귤</S.ItemTitle>
+          <S.ItemPrice>35,000원</S.ItemPrice>
+        </S.Item>
+        <S.Item>
+          <S.ImgContainer>
+            <S.Img />
+          </S.ImgContainer>
+          <S.ItemTitle>애월읍 노지 감귤</S.ItemTitle>
+          <S.ItemPrice>35,000원</S.ItemPrice>
+        </S.Item>
+        <S.Item>
+          <S.ImgContainer>
+            <S.Img />
+          </S.ImgContainer>
+          <S.ItemTitle>애월읍 노지 감귤</S.ItemTitle>
+          <S.ItemPrice>35,000원</S.ItemPrice>
+        </S.Item>
+      </S.Items>
+    </S.Container>
+  );
 }
 
 export default MyPickItems;
--- src/components/Profile/MyPickItems/index.style.js
+++ src/components/Profile/MyPickItems/index.style.js
@@ -1,1 +1,47 @@
 import styled from 'styled-components';
+
+export const Container = styled.section`
+  width: 100%;
+  padding-top: 2rem;
+  background-color: ${({ theme }) => theme.color.WHITE};
+`;
+
+export const Title = styled.p`
+  margin-left: 1.6rem;
+`;
+
+export const Items = styled.ul`
+  display: flex;
+  gap: 1rem;
+  padding: 2rem 1.6rem;
+
+  overflow-x: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const Item = styled.li`
+  flex-shrink: 0;
+  max-width: 14rem;
+`;
+
+export const ImgContainer = styled.div`
+  width: 14rem;
+  height: 9rem;
+  background-color: aqua;
+  border-radius: ${({ theme }) => theme.borderRadius.BASE};
+`;
+
+export const Img = styled.img``;
+
+export const ItemTitle = styled.p`
+  margin-top: 0.5rem;
+`;
+
+export const ItemPrice = styled.p`
+  margin-top: 0.5rem;
+`;
