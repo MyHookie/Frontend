65df184c8f4c5f9797676c0d7f8ae7d05df0cc74
seolha <kynwww@naver.com>
2022-12-27 23:01:30 +0900

    Feat: axios GET 메서드로 myPicks 목록 불러오기(#104)

--- src/components/Profile/MyPickItems/index.jsx
+++ src/components/Profile/MyPickItems/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import * as S from './index.style';
-import imgsrc from '../../../assets/logo.png';
 import MyPickItem from './MyPickItem';
 
 function MyPickItems() {
+  const [myPickItemList, setMyPickItemList] = useState([]);
+
+  const BASE_URL = `https://mandarin.api.weniv.co.kr`;
+
+  const getMyPickItemList = async () => {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}/product/${JSON.parse(
+          localStorage.getItem('accountName')
+        )}`,
+        {
+          headers: {
+            Authorization: `Bearer ${JSON.parse(
+              localStorage.getItem('token')
+            )}`,
+            'Content-type': 'application/json',
+          },
+        }
+      );
+      setMyPickItemList(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <S.Container>
       <S.Title> MyPicks</S.Title>
