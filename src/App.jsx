1d826bc5e917addf2b0aee9205a73643834fc96c
wang-yurin <king_1994@naver.com>
2022-12-30 20:11:44 +0900

    Feat: 라우터 분리 (#141) - 라우터를 하나의 파일로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { useRecoilState } from 'recoil';
-import Login from './pages/Auth/Login';
-import SignUp from './pages/Auth/SignUp';
-import Chat from './pages/Chat';
-import ChatDetail from './pages/Chat/ChatDetail';
-import NotFound from './pages/NotFound';
-import Home from './pages/Home';
-import PostDetail from './pages/Post/PostDetail';
-import PostEdit from './pages/Post/PostEdit';
-import PostUpload from './pages/Post/PostUpload';
-import Profile from './pages/Profile';
-import Follower from './pages/Profile/Follower';
-import Following from './pages/Profile/Following';
-import Splash from './pages/Splash';
-import Welcome from './pages/Welcome';
-import ProfileEdit from './pages/Profile/ProfileEdit';
-import ProfileSetting from './pages/Auth/ProfileSetting';
-import MyPicksUpload from './pages/MyPicksUpload';
-import Search from './pages/Search';
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
+import Router from './routes/Router';
+import GlobalStyle from './styles/GlobalStyle';
+
 import checkTokenValid from './api/tokenValid';
 import loginState from './atoms/login';
-import ScrollToTop from './components/common/ScrollToTop';
+import theme from './styles/Theme';
 
 function App() {
+  const queryClient = new QueryClient();
   const [isLogin, setIsLogin] = useRecoilState(loginState);
 
   useEffect(() => {
@@ -33,36 +21,13 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Splash />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup">
-          <Route index element={<SignUp />} />
-          <Route path="profile" element={<ProfileSetting />} />
-        </Route>
-        <Route path="/home" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/chat/:id" element={<ChatDetail />} />
-        <Route path="/post">
-          <Route path=":id" element={<PostDetail />} />
-          <Route path="upload" element={<PostUpload />} />
-          <Route path="edit/:id" element={<PostEdit />} />
-        </Route>
-        <Route path="/profile/edit" element={<ProfileEdit />} />
-        <Route path="/profile/:accountname">
-          <Route index element={<Profile />} />
-          <Route path="follower" element={<Follower />} />
-          <Route path="following" element={<Following />} />
-        </Route>
-        <Route path="/mypicks" element={<MyPicksUpload />} />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Reset />
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
