bd1a1fce2497e542513f5afd99c9a8a0fa20a069
yoorimseo <seou1225@naver.com>
2022-12-31 17:47:49 +0900

    Design: Following 페이지 스타일 분리 (#147)

--- src/pages/Profile/Following/index.jsx
+++ src/pages/Profile/Following/index.jsx
@@ -1,20 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import styled from 'styled-components';
 import axios from 'axios';
 
+import * as S from './index.styles';
 import BaseHeader from '../../../components/common/BaseHeader';
 import FollowItem from '../../../components/FollowItem';
 
 import arrowIcon from '../../../assets/icon/icon-arrow-left.png';
-import { IR } from '../../../styles/Util';
-
-const SFollowerList = styled.ul`
-  padding: 24px 16px 0;
-  h2 {
-    ${IR}
-  }
-`;
 
 function Following() {
   const navigate = useNavigate();
@@ -56,13 +48,13 @@ function Following() {
         leftClick={handleToProfile}
         title="Followings"
       />
-      <SFollowerList>
+      <S.FollowerList>
         <h2>팔로잉 페이지</h2>
         {followingData.length > 0 &&
           followingData.map((data) => (
             <FollowItem key={data.accountname} data={data} />
           ))}
-      </SFollowerList>
+      </S.FollowerList>
     </>
   );
 }
